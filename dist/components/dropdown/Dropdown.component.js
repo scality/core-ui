41fbc419e963f94982b1d8a9952ea4f52646d8ee
Charles NGUYEN <charles.nguyen@scality.com>
2019-04-11 14:28:47 +0200

    SearchInput: update with hooks

--- dist/components/dropdown/Dropdown.component.js
+++ dist/components/dropdown/Dropdown.component.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
@@ -19,29 +19,17 @@ var defaultTheme = _interopRequireWildcard(require("../../style/theme"));
 
 var _utils = require("../../utils");
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function _templateObject9() {
   var data = _taggedTemplateLiteral(["\n  margin-left: ", ";\n"]);
@@ -161,117 +149,84 @@ var Caret = _styledComponents.default.span(_templateObject9(), defaultTheme.padd
 
 var TriggerStyled = _Button.ButtonStyled.withComponent("div");
 
-var Dropdown =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(Dropdown, _React$Component);
+function Dropdown(_ref) {
+  var items = _ref.items,
+      text = _ref.text,
+      icon = _ref.icon,
+      _ref$size = _ref.size,
+      size = _ref$size === void 0 ? "base" : _ref$size,
+      _ref$variant = _ref.variant,
+      variant = _ref$variant === void 0 ? "primary" : _ref$variant,
+      title = _ref.title,
+      _ref$caret = _ref.caret,
+      caret = _ref$caret === void 0 ? true : _ref$caret;
 
-  function Dropdown() {
-    var _this;
+  var _useState = (0, _react.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      open = _useState2[0],
+      setOpen = _useState2[1];
 
-    _classCallCheck(this, Dropdown);
+  var _useState3 = (0, _react.useState)(),
+      _useState4 = _slicedToArray(_useState3, 2),
+      menuSize = _useState4[0],
+      setMenuSize = _useState4[1];
 
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(Dropdown).call(this));
+  var _useState5 = (0, _react.useState)(),
+      _useState6 = _slicedToArray(_useState5, 2),
+      triggerSize = _useState6[0],
+      setTriggerSize = _useState6[1];
 
-    _defineProperty(_assertThisInitialized(_this), "refMenuCallback", function (element) {
-      if (element) {
-        _this.setState({
-          menuSize: element.getBoundingClientRect()
-        });
-      }
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "refTriggerCallback", function (element) {
-      if (element) {
-        _this.setState({
-          triggerSize: element.getBoundingClientRect()
-        });
-      }
-    });
-
-    _this.state = {
-      open: false,
-      menuSize: null,
-      triggerSize: null
-    };
-    return _this;
-  }
-
-  _createClass(Dropdown, [{
-    key: "handleOpenCloseDropdown",
-    value: function handleOpenCloseDropdown() {
-      this.setState({
-        open: !this.state.open
-      });
+  var refMenuCallback = (0, _react.useCallback)(function (node) {
+    if (node !== null) {
+      setMenuSize(node.getBoundingClientRect());
     }
-  }, {
-    key: "handleOnTriggerClick",
-    value: function handleOnTriggerClick(event) {
-      event.stopPropagation();
+  }, []);
+  var refTriggerCallback = (0, _react.useCallback)(function (node) {
+    if (node !== null) {
+      setTriggerSize(node.getBoundingClientRect());
     }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this2 = this;
-
-      var _this$state = this.state,
-          open = _this$state.open,
-          menuSize = _this$state.menuSize,
-          triggerSize = _this$state.triggerSize;
-      var _this$props = this.props,
-          items = _this$props.items,
-          text = _this$props.text,
-          icon = _this$props.icon,
-          _this$props$size = _this$props.size,
-          size = _this$props$size === void 0 ? "base" : _this$props$size,
-          _this$props$variant = _this$props.variant,
-          variant = _this$props$variant === void 0 ? "primary" : _this$props$variant,
-          title = _this$props.title,
-          _this$props$caret = _this$props.caret,
-          caret = _this$props$caret === void 0 ? true : _this$props$caret;
-      return _react.default.createElement(DropdownStyled, {
-        active: open,
-        variant: variant,
-        className: "sc-dropdown"
-      }, _react.default.createElement(TriggerStyled, {
-        variant: variant,
-        size: size,
-        className: "trigger",
-        onBlur: function onBlur() {
-          return _this2.handleOpenCloseDropdown();
-        },
-        onFocus: function onFocus(event) {
-          return _this2.handleOpenCloseDropdown(event);
-        },
-        onClick: this.handleOnTriggerClick,
-        tabIndex: "0",
-        title: title,
-        ref: this.refTriggerCallback
-      }, icon && _react.default.createElement(_Button.ButtonIcon, {
-        text: text,
-        size: size
-      }, icon), text && _react.default.createElement(_Button.ButtonText, null, text), caret && _react.default.createElement(Caret, null, _react.default.createElement("i", {
-        className: "fas fa-caret-down"
-      })), open && _react.default.createElement(DropdownMenuStyled, {
-        className: "menu-item",
-        postion: "right",
-        ref: this.refMenuCallback,
-        size: menuSize,
-        triggerSize: triggerSize
-      }, items.map(function (item) {
-        var label = item.label,
-            onClick = item.onClick;
-        return _react.default.createElement(DropdownMenuItemStyled, {
-          key: label,
-          onClick: onClick,
-          variant: variant
-        }, label);
-      }))));
-    }
-  }]);
-
-  return Dropdown;
-}(_react.default.Component);
+  }, []);
+  return _react.default.createElement(DropdownStyled, {
+    active: open,
+    variant: variant,
+    className: "sc-dropdown"
+  }, _react.default.createElement(TriggerStyled, {
+    variant: variant,
+    size: size,
+    className: "trigger",
+    onBlur: function onBlur() {
+      return setOpen(!open);
+    },
+    onFocus: function onFocus() {
+      return setOpen(!open);
+    },
+    onClick: function onClick(event) {
+      return event.stopPropagation();
+    },
+    tabIndex: "0",
+    title: title,
+    ref: refTriggerCallback
+  }, icon && _react.default.createElement(_Button.ButtonIcon, {
+    text: text,
+    size: size
+  }, icon), text && _react.default.createElement(_Button.ButtonText, null, text), caret && _react.default.createElement(Caret, null, _react.default.createElement("i", {
+    className: "fas fa-caret-down"
+  })), open && _react.default.createElement(DropdownMenuStyled, {
+    className: "menu-item",
+    postion: "right",
+    ref: refMenuCallback,
+    size: menuSize,
+    triggerSize: triggerSize
+  }, items.map(function (item) {
+    var label = item.label,
+        onClick = item.onClick;
+    return _react.default.createElement(DropdownMenuItemStyled, {
+      key: label,
+      onClick: onClick,
+      variant: variant
+    }, label);
+  }))));
+}
 
 Dropdown.propTypes = {
   size: _propTypes.default.oneOf(["smaller", "small", "base", "large", "larger"]),
