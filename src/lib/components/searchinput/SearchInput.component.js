6beefc3571da288326e0ce02b2b36abb9987a04f
YanJin <yanjin.cheng@scality.com>
2019-09-09 14:12:41 +0200

    check if onReset is define to enable the reset button

--- src/lib/components/searchinput/SearchInput.component.js
+++ src/lib/components/searchinput/SearchInput.component.js
@@ -98,7 +98,10 @@ function SearchInput({
       <SearchIcon onClick={toggle} disabled={!docked}>
         <i className="fas fa-search" />
       </SearchIcon>
-      <ResetIcon onClick={reset} visible={value && !docked}>
+      <ResetIcon
+        onClick={reset}
+        visible={value && !docked && typeof onReset === "function"}
+      >
         <i className="fas fa-times-circle" />
       </ResetIcon>
     </SearchInputContainer>
