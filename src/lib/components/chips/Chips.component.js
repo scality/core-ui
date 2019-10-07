23264633d0910f0dbf5cd8e4cae4a347ee05255b
Charles NGUYEN <charles.nguyen@scality.com>
2019-10-07 09:35:54 +0200

    Chips: improve styles - Add missing import relative to Flow - Improve css - Remove defaultProps - Update tests

--- src/lib/components/chips/Chips.component.js
+++ src/lib/components/chips/Chips.component.js
@@ -1,6 +1,7 @@
 //@flow
 import * as defaultTheme from "../../style/theme";
-
+import type { Variant } from "../constants";
+import type { Node } from "react";
 import styled, { css } from "styled-components";
 
 import React from "react";
@@ -8,34 +9,26 @@ import { lighten } from "polished";
 import { mergeTheme } from "../../utils";
 
 type Props = {
-  text: String,
-  color?: String,
+  text: string,
+  color?: string,
   variant?: Variant,
   icon?: Node,
   buttonIcon?: Node,
   onClick?: any => void,
-  onRemove?: any => void,
-};
-
-const defaultProps = {
-  icon: null,
-  buttonIcon:null,
-  text: "",
+  onRemove?: any => void
 };
 
 const ChipsContainer = styled.div`
   display: inline-flex;
-  background-color:${defaultTheme.grayLighter};
+  background-color: ${defaultTheme.grayLighter};
   border-radius: 15px;
-  cursor: pointer;
 
   ${props => {
     const brandingTheme = mergeTheme(props.theme, defaultTheme);
-    
+
     return css`
       background-color: ${brandingTheme[props.variant]};
       color: ${props.color};
-    
     `;
   }}
 
@@ -43,42 +36,47 @@ const ChipsContainer = styled.div`
     const brandingTheme = mergeTheme(props.theme, defaultTheme);
 
     const brandLight = lighten(0.1, brandingTheme[props.variant]).toString();
-    
-    return props.onClick && css`    
-      &:hover {
-        pointer: cursor
-        background-color: ${brandLight};
-        color: ${props.color};
-      }
-      
-      &:active {
-        background-color: ${brandingTheme[props.variant]};
-        color: ${props.color};
-      }
-    `;
+
+    return (
+      props.onClick &&
+      css`
+        &:hover {
+          cursor: pointer;
+          background-color: ${brandLight};
+          color: ${props.color};
+        }
+
+        &:active {
+          background-color: ${brandingTheme[props.variant]};
+          color: ${props.color};
+        }
+      `
+    );
   }}
 `;
 
 export const ChipsIcon = styled.span`
-  ${props =>     
+  ${props =>
     props.text &&
     css`
       padding-right: ${defaultTheme.padding.smaller};
       display: inline-flex;
       justify-content: center;
       align-items: center;
-      background-color:${lighten(0.15, defaultTheme.brand[props.variant]).toString()};
-      border-radius: 15px;  
-      padding:7px 8px;
-    `
-  }
+      background-color: ${lighten(
+        0.15,
+        mergeTheme(props.theme, defaultTheme)[props.variant]
+      ).toString()};
+      border-radius: 15px;
+      padding: 7px 8px;
+    `}
 `;
 
 export const ChipsButton = styled.span`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  margin-left:7px;
+  margin-left: 7px;
   border-radius: 15px;
   padding: 2px ${defaultTheme.padding.smaller};
 
@@ -86,60 +84,57 @@ export const ChipsButton = styled.span`
     const brandingTheme = mergeTheme(props.theme, defaultTheme);
 
     const brandLight = lighten(0.1, brandingTheme[props.variant]).toString();
-    
-    return props.onClick && css`    
-      &:hover {
-        background-color: ${brandLight};
-        color: ${props.color};
-      }
-      
-      &:active {
-        pointer: cursor
-        background-color: ${brandingTheme[props.variant]};
-        color: ${props.color};
-      }
-    `;
+
+    return (
+      props.onClick &&
+      css`
+        &:hover {
+          background-color: ${brandLight};
+          color: ${props.color};
+          cursor: pointer;
+        }
+
+        &:active {
+          background-color: ${brandingTheme[props.variant]};
+          color: ${props.color};
+        }
+      `
+    );
   }}
-    
 `;
 
 export const ChipsText = styled.span`
   display: inline-flex;
   justify-content: center;
   align-items: center;
-  padding:7px 14px;
-
+  padding: 7px 14px;
 `;
 
 function Chips({
-  text,
+  text = "",
   variant = defaultTheme.brand.base,
-  icon,
-  buttonIcon,
-  onClick, 
+  icon = null,
+  buttonIcon = null,
+  onClick,
   onRemove,
-  color = defaultTheme.white,
+  color = defaultTheme.white
 }: Props) {
   return (
-    <ChipsContainer 
+    <ChipsContainer
       className="sc-chips"
       onClick={onClick}
       variant={variant}
       color={color}
     >
-      { icon && (
+      {icon && (
         <ChipsIcon text={text} variant={variant}>
           {icon}
         </ChipsIcon>
-      )} 
+      )}
       <ChipsText>
         {text}
-        { onRemove && (
-          <ChipsButton
-            onClick={onRemove}
-            color={color}
-            variant={variant}
-          >
+        {onRemove && (
+          <ChipsButton onClick={onRemove} color={color} variant={variant}>
             {buttonIcon}
           </ChipsButton>
         )}
@@ -147,7 +142,5 @@ function Chips({
     </ChipsContainer>
   );
 }
-
-Chips.defaultProps = defaultProps;
 
 export default Chips;
--- stories/chips.js
+++ stories/chips.js
@@ -8,42 +8,42 @@ storiesOf("Chips", module).add("Default", () => {
   return (
     <div className="storybook-chips">
       <h3>Basic Chip</h3>
-      <Chips text="Basic Chip" variant={"primary"} />
-      <Chips text="Basic Chip" variant={"base"} />
-      <Chips text="Basic Chip" variant={"success"} />
-      <Chips text="Basic Chip" variant={"warning"} />
-      <Chips text="Basic Chip" variant={"danger"} />
+      <Chips text="Basic Chip" variant="primary" />
+      <Chips text="Basic Chip" variant="base" />
+      <Chips text="Basic Chip" variant="success" />
+      <Chips text="Basic Chip" variant="warning" />
+      <Chips text="Basic Chip" variant="danger" />
 
-      <h3>Clicable Chip</h3>
+      <h3>Clickable Chip</h3>
       <Chips
-        text="Clicable Chip"
+        text="Clickable Chip"
         icon={<i className="fas fa-star" />}
-        variant={"primary"}
-        onClick={action("Clicable Chip")}
+        variant="primary"
+        onClick={action("Clickable Chip")}
       />
       <Chips
-        text="Clicable Chip"
+        text="Clickable Chip"
         icon={<i className="fas fa-star" />}
-        variant={"base"}
-        onClick={action("Clicable Chip")}
+        variant="base"
+        onClick={action("Clickable Chip")}
       />
       <Chips
-        text="Clicable Chip"
+        text="Clickable Chip"
         icon={<i className="fas fa-star" />}
-        variant={"success"}
-        onClick={action("Clicable Chip")}
+        variant="success"
+        onClick={action("Clickable Chip")}
       />
       <Chips
-        text="Clicable Chip"
+        text="Clickable Chip"
         icon={<i className="fas fa-star" />}
-        variant={"warning"}
-        onClick={action("Clicable Chip")}
+        variant="warning"
+        onClick={action("Clickable Chip")}
       />
       <Chips
-        text="Clicable Chip"
+        text="Clickable Chip"
         icon={<i className="fas fa-star" />}
-        variant={"danger"}
-        onClick={action("Clicable Chip")}
+        variant="danger"
+        onClick={action("Clickable Chip")}
       />
 
       <h3>Deletable Chip</h3>
@@ -51,35 +51,35 @@ storiesOf("Chips", module).add("Default", () => {
         text="Deletable"
         icon={<i className="fas fa-star" />}
         buttonIcon={<i className="fas fa-times" />}
-        variant={"primary"}
+        variant="primary"
         onRemove={action("Deletable Chip")}
       />
       <Chips
         text="Deletable"
         icon={<i className="fas fa-star" />}
         buttonIcon={<i className="fas fa-times" />}
-        variant={"base"}
+        variant="base"
         onRemove={action("Deletable Chip")}
       />
       <Chips
         text="Deletable"
         icon={<i className="fas fa-star" />}
         buttonIcon={<i className="fas fa-times" />}
-        variant={"success"}
+        variant="success"
         onRemove={action("Deletable Chip")}
       />
       <Chips
         text="Deletable"
         icon={<i className="fas fa-star" />}
         buttonIcon={<i className="fas fa-times" />}
-        variant={"warning"}
+        variant="warning"
         onRemove={action("Deletable Chip")}
       />
       <Chips
         text="Deletable"
         icon={<i className="fas fa-star" />}
         buttonIcon={<i className="fas fa-times" />}
-        variant={"danger"}
+        variant="danger"
         onRemove={action("Deletable Chip")}
       />
     </div>
