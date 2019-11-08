11e72737e14f69b694aa51b82728f617bff483a4
edumoreira1506 <contato@eduardoem.com.br>
2019-11-08 13:24:50 -0300

    Removed logic from component

--- src/lib/components/progressbar/ProgressBar.component.js
+++ src/lib/components/progressbar/ProgressBar.component.js
@@ -1,56 +1,52 @@
 //@flow
 import React, { useEffect, useState } from 'react';
 import styled, { css } from "styled-components";
+import { mergeTheme } from "../../utils";
+import * as defaultTheme from "../../style/theme";
 
 export type ProgressBarProps = {
-  total: number,
-  filled: number,
+  percentage: number,
   size?: Size,
-  color: string,
-  measure: string,
-  percentage?: boolean,
-  capacity?: boolean,
-  label?: boolean
-};
+  color?: string,
+  topLeftLabel?: string,
+  topRightLabel?: string,
+  bottomLeftLabel?: string,
+  bottomRightLabel?: string,
+}
 
 export const ProgressBarContainer = styled.div`
-  display: block;
-  background: #d9d9d9;
+  background: ${defaultTheme.gray};
+  width: 100%;
 
   ${props => {
     switch (props.size) {
       case "smaller":
         return css`
           border-radius: 12px;
-          width: 200px;
           height: 10px;
         `;
 
       case "small":
         return css`
           border-radius: 12px;
-          width: 250px;
           height: 15px;
         `;
 
       case "large":
         return css`
           border-radius: 12px;
-          width: 300px;
           height: 15px;
         `;
 
       case "larger":
         return css`
           border-radius: 12px;
-          width: 400px;
           height: 20px;
         `;
 
       default:
         return css`
           border-radius: 12px;
-          width: 200px;
           height: 10px;
         `;
   }
@@ -88,40 +84,11 @@ export const Container = styled.div`
   }
 
   margin: 10px 0 10px 0;
-
-  ${props => {
-    switch (props.size) {
-      case "smaller":
-        return css`
-          width: 200px;
-        `;
-
-      case "small":
-        return css`
-          width: 250px;
-        `;
-
-      case "large":
-        return css`
-          width: 300px;
-        `;
-
-      case "larger":
-        return css`
-          width: 400px;
-        `;
-
-      default:
-        return css`
-          width: 200px;
-        `;
-  }
-}}
 `;
 
 export const LabelContainer = styled.div`
   margin-top: 6px;
-  font-size: 14px;
+  font-size: ${defaultTheme.base};
 `;
 
 export const FilledAreaContainer = styled.div`
@@ -131,79 +98,41 @@ export const FilledAreaContainer = styled.div`
   -o-transition:All 1s ease;
 
   ${props => {
-    let borderStyle;
-
-    switch (props.size) {
-      case "smaller":
-        borderStyle = `
-          border-radius: 12px;
-        `;
-        break;
-
-      case "small":
-        borderStyle = `
-          border-radius: 12px;
-        `;
-        break;
-
-      case "large":
-        borderStyle = `
-          border-radius: 12px;
-        `;
-        break;
-
-      case "larger":
-        borderStyle = `
-          border-radius: 12px;
-        `;
-        break;
-
-      default:
-        borderStyle = `
-          border-radius: 12px;
-        `;
-        break;
-    }
-
     return css`
-      background: ${props.color};
+      border-radius: 12px;
+      background-color: ${props => props.color || mergeTheme(props.theme, defaultTheme).primary};
       width: ${props.width}%;
       height: 100%;
-      ${borderStyle}
     `;
   }
 }}
 `;
 
 function ProgressBar({
-  total = 100,
-  filled = 50,
+  percentage = 50,
   size = 'base',
   color = '#fcb039',
-  measure = '',
-  percentage = false,
-  capacity = false,
-  label = false
-}) {
+  topLeftLabel = '55%',
+  topRightLabel = '80GB Total',
+  bottomLeftLabel = '45GB Used',
+  bottomRightLabel = '25GB Free'
+} : ProgressBarProps) {
   const [width, setWidth] = useState(0);
-  const filledWidth = calcWidth(total, filled);
-  useEffect(() => setWidth(filledWidth), [filledWidth]);
+  useEffect(() => setWidth(percentage), [percentage]);
 
   return (
     <Container
       size={size}
     >
       {
-        percentage &&
-          <span className="percentage">
-            {filledWidth}%
-          </span>
+        <span className="percentage">
+          {topLeftLabel}
+        </span>
       }
       {
-        capacity &&
-          <span className="capacity">
-            {`${total} ${measure} Total`}
-          </span>
+        <span className="capacity">
+          {topRightLabel}
+        </span>
       }
       <ProgressBarContainer
         size={size}
@@ -214,22 +143,17 @@ function ProgressBar({
         />
       </ProgressBarContainer>
       {
-        label &&
-        (
-          <LabelContainer>
-            <span className="used">
-              {`${filled} ${measure}`} used
-            </span>
-            <span className="free">
-              {`${total - filled} ${measure}`} free
-            </span>
-          </LabelContainer>
-        )
+        <LabelContainer>
+          <span className="used">
+            {bottomLeftLabel}
+          </span>
+          <span className="free">
+            {bottomRightLabel}
+          </span>
+        </LabelContainer>
       }
     </Container>
   )
 }
-
-const calcWidth = (total, filled) => (100 * filled / total).toFixed(2);
 
 export default ProgressBar;
--- stories/progressbar.js
+++ stories/progressbar.js
@@ -7,117 +7,117 @@ import { Wrapper, Title } from "./common";
 storiesOf("ProgressBar", module).add("Default", () => {
   return (
     <Wrapper>
-      <Title>Smaller</Title>
-      <ProgressBar 
-        size='smaller'
-        percentage={true}
-        capacity={true}
-        measure='GB'
-        label={true}
-      />
+      <div style={{width: '200px'}}>
+        <Title>Smaller</Title>
+        <ProgressBar 
+          size='smaller'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
 
-      <Title>Small</Title>
-      <ProgressBar 
-        size='small'
-        percentage={true}
-        capacity={true}
-        label={true}
-        measure='GB'
-      />
+        <Title>Small</Title>
+        <ProgressBar 
+          size='small'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
 
-      <Title>Large</Title>
-      <ProgressBar 
-        size='large'
-        measure='GB'
-        percentage={true}
-        label={true}
-        capacity={true}
-      />
+        <Title>Large</Title>
+        <ProgressBar 
+          size='large'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
 
-      <Title>Larger</Title>
-      <ProgressBar 
-        size='smaller'
-        measure='GB'
-        percentage={true}
-        label={true}
-        capacity={true}
-      />
+        <Title>Larger</Title>
+        <ProgressBar 
+          size='larger'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
 
-      <Title>Different colors</Title>
-      <ProgressBar 
-        size='smaller'
-        measure='GB'
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#2f67ac'
-      />
-      <ProgressBar 
-        size='smaller'
-        measure='GB'
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#ff5722'
-      />
-      <ProgressBar 
-        size='smaller'
-        measure='GB'
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#100704'
-      />
-      <ProgressBar 
-        size='smaller'
-        measure='GB'
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#982803'
-      />
-
-      <Title>Different measures/capacity</Title>
-      <ProgressBar 
-        size='smaller'
-        measure='KG'
-        total={300}
-        filled={72}
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#2f67ac'
-      />
-      <ProgressBar 
-        size='smaller'
-        measure='CM'
-        total={500}
-        filled={347}
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#ff5722'
-      />
-      <ProgressBar 
-        size='smaller'
-        measure='people'
-        total={10000}
-        filled={456}
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#100704'
-      />
-      <ProgressBar 
-        size='smaller'
-        measure='M'
-        total={3}
-        filled={1}
-        percentage={true}
-        label={true}
-        capacity={true}
-        color='#982803'
-      />
+        <Title>Different colors</Title>
+        <ProgressBar 
+          size='smaller'
+          color='#2f67ac'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
+        <ProgressBar 
+          size='smaller'
+          color='#ff5722'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
+        <ProgressBar 
+          size='smaller'
+          color='#100704'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
+        <ProgressBar 
+          size='smaller'
+          color='#982803'
+          percentage={50}
+          topLeftLabel='50%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='50GB Used'
+          bottomRightLabel='50GB Free'
+        />
+        <ProgressBar 
+          color='#2f67ac'
+          percentage={24}
+          topLeftLabel='24%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='24GB Used'
+          bottomRightLabel='76GB Free'
+        />
+        <ProgressBar 
+          color='#ff5722'
+          percentage={70}
+          topLeftLabel='70%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='70GB Used'
+          bottomRightLabel='30GB Free'
+        />
+        <ProgressBar 
+          color='#100704'
+          percentage={5}
+          topLeftLabel='5%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='5GB Used'
+          bottomRightLabel='95GB Free'
+        />
+        <ProgressBar 
+          color='#982803'
+          percentage={78}
+          topLeftLabel='78%'
+          topRightLabel='100GB Total'
+          bottomLeftLabel='78GB Used'
+          bottomRightLabel='22GB Free'
+        />
+      </div>
     </Wrapper>
   )
 });
