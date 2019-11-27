6928519f0ce6864859b81f43ce287e4f335e5b5a
Stheffany Hadlich <stheffany@codeminer42.com>
2019-11-27 20:14:46 -0300

    Create base to collapsiblePanel

---
+++ src/lib/components/collapsiblepanel/CollapsiblePanel.component.js
@@ -0,0 +1,18 @@
+//@flow
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+};
+
+const CollapsiblePanelContainer = styled.div``;
+
+function CollapsiblePanel(props: Props) {
+  return (
+    <CollapsiblePanelContainer className="sc-collapsiblepanel">
+     
+    </CollapsiblePanelContainer>
+  );
+}
+
+export default CollapsiblePanel;
