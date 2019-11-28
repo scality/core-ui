9f7a11c8850a3fc0dc734ec78c217a3f3f8f2587
Stheffany Hadlich <stheffany@codeminer42.com>
2019-11-28 00:01:02 -0300

    Create expandaded and colapsed versions

--- src/lib/components/collapsiblepanel/CollapsiblePanel.component.js
+++ src/lib/components/collapsiblepanel/CollapsiblePanel.component.js
@@ -1,16 +1,112 @@
 //@flow
 import React from "react";
-import styled from "styled-components";
+import { lighten } from "polished";
+import styled, { css } from "styled-components";
+import * as defaultTheme from "../../style/theme";
+import Button from "../button/Button.component";
+import { mergeTheme } from "../../utils";
 
 type Props = {
+  expand?: Boolean,
+  onHeaderClick?: any => void,
+  onClick? : any => void,
+  children?: Array,
+  node?: String,
+  variant? : string,
 };
 
-const CollapsiblePanelContainer = styled.div``;
+const ARROW = {
+  colapsed: 'fas fa-angle-down',
+  expanded: 'fas fa-angle-up',
+}
 
-function CollapsiblePanel(props: Props) {
+const PanelText = styled.div`
+`;
+
+const CollapsiblePanelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px 8px 4px 4px;
+
+  ${props => {
+    const brandingTheme = mergeTheme(props.theme, defaultTheme);
+
+    return css`
+        background-color: ${brandingTheme[props.variant]};
+        color: ${brandingTheme.background};
+      `;
+    }
+  }
+`;
+
+
+const HeaderContainer = styled.div`
+  height: ${defaultTheme.navbarHeight};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${defaultTheme.padding.small} ${defaultTheme.padding.large}
+`;
+
+const ExpandedItems = styled.div`
+  height: ${defaultTheme.navbarHeight};
+  display: flex;
+  align-items: center;
+  padding: ${defaultTheme.padding.smaller} ${defaultTheme.padding.large}
+
+  ${props => {
+    const brandingTheme = mergeTheme(props.theme, defaultTheme);
+    const brandLight = lighten(0.1,  brandingTheme.background).toString();
+    return props.onClick
+      && css`
+        &:hover {
+          cursor: pointer;
+          background-color: ${brandLight};
+        }
+      `;
+    }
+  }
+`;
+
+const ExpandedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+function CollapsiblePanel({
+  expanded = false,
+  variant =  'primary',
+  onHeaderClick,
+  onClick,
+  children = [],
+  node = '',
+}: Props) {
   return (
-    <CollapsiblePanelContainer className="sc-collapsiblepanel">
-     
+    <CollapsiblePanelContainer className="sc-collapsiblepanel" variant={variant} >
+      <HeaderContainer>
+        <PanelText className="sc-chips-text">
+          {node}
+        </PanelText>
+        <Button
+          size="larger"
+          variant={variant}
+          onClick={onHeaderClick}
+          icon={<i className={expanded ? ARROW.expanded : ARROW.colapsed} />}
+        />
+      </HeaderContainer>
+      {
+        expanded && (
+          <ExpandedContainer>
+            {
+              children.map(item =>
+                <ExpandedItems key={item} onClick={onClick} variant={variant}>
+                  {item}
+                </ExpandedItems>
+              )
+            }
+          </ExpandedContainer>
+        )
+      }
     </CollapsiblePanelContainer>
   );
 }
--- stories/collapsiblepanel.js
+++ stories/collapsiblepanel.js
@@ -1,13 +1,31 @@
+import { Title, Wrapper } from "./common";
+
+import CollapsiblePanel from "../src/lib/components/collapsiblepanel/CollapsiblePanel.component";
 //@flow
 import React from "react";
+import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
-import CollapsiblePanel from "../src/lib/components/collapsiblepanel/CollapsiblePanel.component";
+
+const items = ['banana', 'apple', 'grape'];
 
 storiesOf("CollapsiblePanel", module)
   .add("Default", () => {
     return (
-      <div>
-          <CollapsiblePanel />
-      </div>
+      <Wrapper className="storybook-chips">
+        <Title>Colapsed</Title>
+        <CollapsiblePanel
+          node="Colapsed Panel"
+          onHeaderClick={action("Expand panel")}
+          children={items}
+        />
+
+        <Title>Expanded</Title>
+        <CollapsiblePanel
+          node="Expanded Panel"
+          expanded
+          onHeaderClick={action("Colapse panel")}
+          children={items}
+        />
+      </Wrapper>
     );
   });
