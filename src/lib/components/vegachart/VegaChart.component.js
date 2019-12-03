56eeb1b863c4bf1991a6f62610238c0b127ae301
YanJin <yanjin.cheng@scality.com>
2019-12-03 17:05:05 +0100

    sparkline: add the title in vegachart in orger to change the theme
    Refs: #108

--- src/lib/components/vegachart/VegaChart.component.js
+++ src/lib/components/vegachart/VegaChart.component.js
@@ -18,17 +18,38 @@ function VegaChart({ id, spec }: Props) {
     themeContext.brand &&
     themeContext.brand.backgroundContrast1;
 
+  const currentBackgroundColor2 =
+    themeContext &&
+    themeContext.brand &&
+    themeContext.brand.backgroundContrast2;
+
+  const brandText =
+    themeContext && themeContext.brand && themeContext.brand.text;
+
   const themeConfig = {
     config: {
       background: currentBackgroundColor,
       axis: {
-        labelColor:
-          themeContext && themeContext.brand && themeContext.brand.text,
-        titleColor:
-          themeContext && themeContext.brand && themeContext.brand.text,
+        labelColor: brandText,
+        titleColor: brandText,
         grid: false
       },
-      view: { stroke: "transparent" }
+      title: {
+        color: brandText
+      },
+      view: { stroke: "transparent", fill: currentBackgroundColor2 },
+      // the headers provide a title and labels for faceted plots.
+      header: {
+        labelColor: brandText
+      },
+      // the label of max/min
+      text: {
+        color: brandText
+      },
+      // the trend line max/min and verticle
+      rule: {
+        color: brandText
+      }
     }
   };
   const themedSpec = { ...spec, ...themeConfig };
