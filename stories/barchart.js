da18085153db5edd24ad1219ccdcf813169db4f9
YanJin <yanjin.cheng@scality.com>
2020-01-21 17:10:52 +0100

    barchart: improve the x-axis label

--- stories/barchart.js
+++ stories/barchart.js
@@ -12,10 +12,13 @@ import {
 // props for vertical stacked bar chart
 const idVerticalStacked = "vis_vertical_stacked";
 const xAxisVerticalStacked = {
-  timeUnit: "yearmonthdate",
-  field: "date",
+  field: "xlabel",
   type: "ordinal",
-  title: null
+  title: null,
+  axis: { labelAngle: 0 },
+  sort: {
+    order: "ascending"
+  }
 };
 const yAxisVerticalStacked = {
   aggregate: "count",
@@ -29,13 +32,19 @@ const yAxisVerticalStacked = {
 const colorVerticalStacked = {
   field: "status",
   type: "nominal",
-  legend: { direction: "horizontal", orient: "top", title: null },
+  legend: {
+    direction: "horizontal",
+    orient: "top",
+    title: null,
+    symbolType: "circle",
+    columnPadding: 50
+  },
   scale: {
     domain: ["2XX", "401", "404", "4XX", "503", "5XX"],
     range: ["#4BE4E2", "#E45834", "#FEFA52", "#968BFF", "#BE2543", "#DC90F1"]
   }
 };
-const width = 1000;
+const width = 800;
 // the size control the size of each small item of the bar
 const barConfig = { cornerRadius: 8, size: 12 };
 
