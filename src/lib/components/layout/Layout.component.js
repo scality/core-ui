5d87a5dc11fbcdecab62d54bdf88c2c9edff643c
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2021-02-11 12:26:56 +0100

    layout: Accept a new navbarElement property to allow navbar customization

--- src/lib/components/layout/Layout.component.js
+++ src/lib/components/layout/Layout.component.js
@@ -9,9 +9,10 @@ import type { Props as SidebarProps } from "../sidebar/Sidebar.component";
 import Sidebar from "../sidebar/Sidebar.component";
 import { getThemePropSelector } from "../../utils";
 type Props = {
-  navbar: NavbarProps,
-  sidebar: SidebarProps,
-  children: Node,
+  navbar?: NavbarProps,
+  sidebar?: SidebarProps,
+  navbarElement?: Node,
+  children: Node
 };
 
 const LayoutContainer = styled.div`
@@ -32,10 +33,11 @@ const MainContent = styled.div`
   background-color: ${getThemePropSelector("background")};
 `;
 
-function Layout({ children, sidebar, navbar, ...rest }: Props) {
+function Layout({ children, sidebar, navbar, navbarElement, ...rest }: Props) {
   return (
     <LayoutContainer className="sc-layout" {...rest}>
       {navbar && <Navbar {...navbar} />}
+      {!navbar && navbarElement !== undefined && navbarElement !== null && navbarElement}
       <ContentContainer>
         {sidebar && <Sidebar {...sidebar} />}
         <MainContent className="main">{children}</MainContent>
