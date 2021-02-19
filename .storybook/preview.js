13ba7fe22b15c8230f8c118f3ffa56998c3a2a99
Patrick Ear <patrick.ear@scality.com>
2021-02-19 18:09:28 +0100

    deps: upgrade storybook to v6.1

---
+++ .storybook/preview.js
@@ -0,0 +1,34 @@
+import { addDecorator } from "@storybook/react";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import {
+  gray,
+  blueLighter,
+  jade,
+  turquoise,
+  yellowOrange,
+  warmRed,
+  white,
+  defaultTheme,
+} from "../src/lib/style/theme";
+
+const themes = [
+  {
+    name: "Dark Theme",
+    brand: defaultTheme.dark,
+  },
+  {
+    name: "Light Theme",
+    brand: defaultTheme.light,
+  },
+];
+addDecorator(withThemesProvider(themes));
+// addDecorator(
+//   withInfo({
+//     header: false,
+//     inline: false,
+//     maxPropArrayLength: 10,
+//   }),
+// );
+
+// const themes = [theme1, theme2];
+// addDecorator(withThemesProvider(themes));
