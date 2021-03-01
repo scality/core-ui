fda2470b6610463ef40b3ad2bbc5ca01bba78771
Patrick Ear <patrick.ear@scality.com>
2021-03-01 17:51:39 +0100

    stucture: add .stories to stories files
    We add this ".stories" ending at the end of stories files because
    with MDX, we might want to render custom component.
    
    Because of the new way storybook CSF work, anything that is exported
    in a file in /stories/* will be displayed in storybook.
    
    We want a way to discriminate files in the stories/ folder that will not
    be displayed by storybook.
    That's why I change the `main.js` in order to filtrate this kind of files.
    Now you will be able to create custom component to render in your MDX files.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ["../stories/*.@(js|mdx)"],
+  stories: ["../stories/**/*.stories.@(js|mdx)"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-knobs"],
 };
