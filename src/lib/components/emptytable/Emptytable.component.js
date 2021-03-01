dea259966ccc1bb5629748072636af8623cfc843
Alexis Le Dinh <alexisledinh@gmail.com>
2021-03-01 10:01:48 +0100

    EmptyTable: Add useDiv prop to return div instead of tr for div table context

--- src/lib/components/emptytable/Emptytable.component.js
+++ src/lib/components/emptytable/Emptytable.component.js
@@ -4,8 +4,10 @@ import styled, { css } from "styled-components";
 import * as defaultTheme from "../../style/theme";
 import { getTheme } from "../../utils";
 
+// useDiv returns a div wrapper instead of tr more adapted to div based table (instead of tbody/thead/...)
 type Props = {
   children: Node | React.Node,
+  useDiv?: boolean,
 };
 
 const EmptytableContainer = styled.tr`
@@ -24,8 +26,36 @@ const EmptytableContainer = styled.tr`
   }}
 `;
 
+const EmptytableContainerDiv = styled.div`
+  width: 100%;
+  padding: ${defaultTheme.padding.large};
+  box-sizing: border-box;
+  display: table;
+  table-layout: fixed;
+
+  ${(props) => {
+    const brand = getTheme(props);
+    return css`
+      background: ${brand.primary};
+      border-top: 1px solid ${brand.border};
+    `;
+  }}
+`;
+
+// Using !important as the only way to prioritize this over the general tr td styling that may be defined in the apps
 const EmptytableContent = styled.td`
-  // Using !important as the only way to prioritize this over the general tr td styling that may be defined in the apps
+  text-align: center !important;
+  border: none !important;
+  ${(props) => {
+    const brand = getTheme(props);
+    return css`
+      background: ${brand.primary};
+      color: ${brand.textSecondary};
+    `;
+  }}
+`;
+
+const EmptytableContentDiv = styled.div`
   text-align: center !important;
   border: none !important;
   ${(props) => {
@@ -38,6 +68,13 @@ const EmptytableContent = styled.td`
 `;
 
 function Emptytable(props: Props) {
+  if (props.useDiv)
+    return (
+      <EmptytableContainerDiv className="sc-emptytable">
+        <EmptytableContentDiv>{props.children}</EmptytableContentDiv>
+      </EmptytableContainerDiv>
+    );
+
   return (
     <EmptytableContainer className="sc-emptytable">
       <EmptytableContent>{props.children}</EmptytableContent>
