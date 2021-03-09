ce1f1ff2135e91353a94ed2bcdff8f6175210243
YanJin <yanjin.cheng@scality.com>
2021-03-09 12:22:44 +0100

    Auto formatting
    Refs: #261

--- src/lib/style/theme.js
+++ src/lib/style/theme.js
@@ -1,113 +1,113 @@
 //== Colors
-export const hotPink = "#E40046";
-export const pink = "#EB4962";
-export const lightPink = "#F5B5BA";
-export const plum = "#8A1B61";
-export const jade = "#006F62";
-export const lightBeige = "#FAF7F5";
-export const warmRed = "#EF3340";
-export const turquoise = "#00B2A9";
-export const yellowOrange = "#F1B434";
-export const mediumOrange = "#FF8F1C";
-export const yellow = "#EFC93D";
+export const hotPink = '#E40046';
+export const pink = '#EB4962';
+export const lightPink = '#F5B5BA';
+export const plum = '#8A1B61';
+export const jade = '#006F62';
+export const lightBeige = '#FAF7F5';
+export const warmRed = '#EF3340';
+export const turquoise = '#00B2A9';
+export const yellowOrange = '#F1B434';
+export const mediumOrange = '#FF8F1C';
+export const yellow = '#EFC93D';
 
-export const green = "#228F67";
-export const white = "#ffffff";
-export const black = "#000000";
-export const blackLight = "#313B44";
+export const green = '#228F67';
+export const white = '#ffffff';
+export const black = '#000000';
+export const blackLight = '#313B44';
 
-export const blueDarkest = "#0F3554";
-export const blueDarker = "#144A75";
-export const blueDark = "#1F73B7";
-export const blue = "#337FBD";
-export const blueLight = "#5293C7";
-export const blueLighter = "#ADCCE4";
-export const blueLightest = "#CEE2F2";
+export const blueDarkest = '#0F3554';
+export const blueDarker = '#144A75';
+export const blueDark = '#1F73B7';
+export const blue = '#337FBD';
+export const blueLight = '#5293C7';
+export const blueLighter = '#ADCCE4';
+export const blueLightest = '#CEE2F2';
 
-export const grayDarkest = "#2F3941";
-export const grayDarker = "#49545C";
-export const grayDark = "#68737D";
-export const gray = "#87929D";
-export const grayLight = "#C2C8CC";
-export const grayLighter = "#D8DCDE";
-export const grayLightest = "#E9EBED";
+export const grayDarkest = '#2F3941';
+export const grayDarker = '#49545C';
+export const grayDark = '#68737D';
+export const gray = '#87929D';
+export const grayLight = '#C2C8CC';
+export const grayLighter = '#D8DCDE';
+export const grayLightest = '#E9EBED';
 
 // Export the default light & dark theme
 export const defaultTheme = {
   light: {
-    alert: "#A39300",
-    base: "#607080",
-    primary: "#FAF9FB",
-    primaryDark1: "#F7F6F9",
-    primaryDark2: "#EDEAF0",
-    secondary: "#0F7FFF",
-    secondaryDark1: "#1C3D59",
-    secondaryDark2: "#1C2E3F",
-    success: "#006F62",
-    healthy: "#24871D",
-    healthyLight: "#33A919",
-    warning: "#946F00",
-    danger: "#AA1D05",
-    critical: "#BE321F",
-    background: "#ffffff",
-    backgroundBluer: "#ECF4FF",
-    textPrimary: "#313B44",
-    textSecondary: "#8593A0",
-    textTertiary: "#A7B6C3",
-    borderLight: "#EBEBEB",
-    border: "#A5A5A5",
-    info: "#8C8C8C",
+    alert: '#A39300',
+    base: '#607080',
+    primary: '#FAF9FB',
+    primaryDark1: '#F7F6F9',
+    primaryDark2: '#EDEAF0',
+    secondary: '#0F7FFF',
+    secondaryDark1: '#1C3D59',
+    secondaryDark2: '#1C2E3F',
+    success: '#006F62',
+    healthy: '#24871D',
+    healthyLight: '#33A919',
+    warning: '#946F00',
+    danger: '#AA1D05',
+    critical: '#BE321F',
+    background: '#ffffff',
+    backgroundBluer: '#ECF4FF',
+    textPrimary: '#313B44',
+    textSecondary: '#8593A0',
+    textTertiary: '#A7B6C3',
+    borderLight: '#EBEBEB',
+    border: '#A5A5A5',
+    info: '#8C8C8C',
   },
   dark: {
-    alert: "#FFE508",
-    base: "#7B7B7B",
-    primary: "#1D1D1D",
-    primaryDark1: "#171717",
-    primaryDark2: "#0A0A0A",
-    secondary: "#0F7FFF",
-    secondaryDark1: "#1C3D59",
-    secondaryDark2: "#1C2E3F",
-    success: "#006F62",
-    healthy: "#30AC26",
-    healthyLight: "#69E44C",
-    warning: "#FFC10A",
-    danger: "#AA1D05",
-    critical: "#BE321F",
-    background: "#121212",
-    backgroundBluer: "#192A41",
-    textPrimary: "#FFFFFF",
-    textSecondary: "#B5B5B5",
-    textTertiary: "#DFDFDF",
-    borderLight: "#A5A5A5",
-    border: "#313131",
-    info: "#434343",
+    alert: '#FFE508',
+    base: '#7B7B7B',
+    primary: '#1D1D1D',
+    primaryDark1: '#171717',
+    primaryDark2: '#0A0A0A',
+    secondary: '#0F7FFF',
+    secondaryDark1: '#1C3D59',
+    secondaryDark2: '#1C2E3F',
+    success: '#006F62',
+    healthy: '#30AC26',
+    healthyLight: '#69E44C',
+    warning: '#FFC10A',
+    danger: '#AA1D05',
+    critical: '#BE321F',
+    background: '#121212',
+    backgroundBluer: '#192A41',
+    textPrimary: '#FFFFFF',
+    textSecondary: '#B5B5B5',
+    textTertiary: '#DFDFDF',
+    borderLight: '#A5A5A5',
+    border: '#313131',
+    info: '#434343',
   },
 };
 export const brand = defaultTheme.dark;
 
 //navbar
-export const navbarHeight = "47px";
-export const navbarItemWidth = "60px";
+export const navbarHeight = '47px';
+export const navbarItemWidth = '60px';
 
 //sidebar
-export const sidebarItemHeight = "40px";
-export const sidebarWidth = "40px";
+export const sidebarItemHeight = '40px';
+export const sidebarWidth = '40px';
 
 export const fontSize = {
-  smaller: "10px",
-  small: "12px",
-  base: "14px",
-  large: "16px",
-  larger: "20px",
-  huge: "22px",
-  massive: "24px",
+  smaller: '10px',
+  small: '12px',
+  base: '14px',
+  large: '16px',
+  larger: '20px',
+  huge: '22px',
+  massive: '24px',
 };
 
 export const fontWeight = {
-  light: "400",
-  base: "400",
-  semibold: "600",
-  bold: "700",
+  light: '400',
+  base: '400',
+  semibold: '600',
+  bold: '700',
 };
 
 export const padding = {
@@ -120,13 +120,13 @@ export const padding = {
 
 // Svg size
 export const svgSize = {
-  smaller: "11px",
-  small: "14px",
-  base: "16px",
-  large: "18px",
-  larger: "22px",
-  huge: "30px",
-  massive: "40px",
+  smaller: '11px',
+  small: '14px',
+  base: '16px',
+  large: '18px',
+  larger: '22px',
+  huge: '30px',
+  massive: '40px',
 };
 export const zIndex = {
   tooltip: 9990,
