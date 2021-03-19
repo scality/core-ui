96f16f31b2f8bccc2a3122fc9a872edf0b298de9
Alexis Le Dinh <alexisledinh@gmail.com>
2021-03-19 17:05:01 +0100

    Layout: custom styled scrollbar for Layout children components

--- src/lib/components/layout/Layout.component.js
+++ src/lib/components/layout/Layout.component.js
@@ -1,24 +1,64 @@
 //@flow
-import React from "react";
-import styled from "styled-components";
-import type { Node } from "react";
-import { navbarHeight } from "../../style/theme";
-import Navbar from "../navbar/Navbar.component";
-import type { Props as NavbarProps } from "../navbar/Navbar.component";
-import type { Props as SidebarProps } from "../sidebar/Sidebar.component";
-import Sidebar from "../sidebar/Sidebar.component";
-import { getThemePropSelector } from "../../utils";
+import React from 'react';
+import styled, { css } from 'styled-components';
+import type { Node } from 'react';
+import { navbarHeight } from '../../style/theme';
+import Navbar from '../navbar/Navbar.component';
+import type { Props as NavbarProps } from '../navbar/Navbar.component';
+import type { Props as SidebarProps } from '../sidebar/Sidebar.component';
+import Sidebar from '../sidebar/Sidebar.component';
+import { getThemePropSelector, getTheme } from '../../utils';
 type Props = {
   navbar?: NavbarProps,
   sidebar?: SidebarProps,
   navbarElement?: Node,
-  children: Node
+  children: Node,
 };
 
 const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+
+  ${(props) => {
+    const brand = getTheme(props);
+    return css`
+      // Custom scrollbar
+      * {
+        // Chrome / Safari / Edge
+        ::-webkit-scrollbar {
+          width: 8px;
+        }
+
+        ::-webkit-scrollbar-track {
+          background: ${brand.primary};
+        }
+
+        ::-webkit-scrollbar-thumb {
+          width: 4px;
+          background: ${brand.border};
+          border-radius: 10px;
+          -webkit-border-radius: 10px;
+          background-clip: padding-box;
+          border-left: 3px solid rgba(0, 0, 0, 0);
+          border-right: 1px solid rgba(0, 0, 0, 0);
+        }
+
+        ::-webkit-scrollbar-button {
+          width: 0;
+          height: 0;
+          display: none;
+        }
+        ::-webkit-scrollbar-corner {
+          background-color: transparent;
+        }
+
+        // Firefox
+        scrollbar-color: ${brand.border} ${brand.primary};
+        scrollbar-width: thin;
+      }
+    `;
+  }}
 `;
 
 const ContentContainer = styled.div`
@@ -30,14 +70,17 @@ const ContentContainer = styled.div`
 
 const MainContent = styled.div`
   flex-grow: 1;
-  background-color: ${getThemePropSelector("background")};
+  background-color: ${getThemePropSelector('background')};
 `;
 
 function Layout({ children, sidebar, navbar, navbarElement, ...rest }: Props) {
   return (
     <LayoutContainer className="sc-layout" {...rest}>
       {navbar && <Navbar {...navbar} />}
-      {!navbar && navbarElement !== undefined && navbarElement !== null && navbarElement}
+      {!navbar &&
+        navbarElement !== undefined &&
+        navbarElement !== null &&
+        navbarElement}
       <ContentContainer>
         {sidebar && <Sidebar {...sidebar} />}
         <MainContent className="main">{children}</MainContent>
