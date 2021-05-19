625b8640df872df74579d0d25e7ac6f0e931e746
theocerutti <theo.cerutti@epitech.eu>
2021-05-19 11:44:29 +0200

    Toggle: go back to the last toggle

--- src/lib/components/toggle/Toggle.component.js
+++ src/lib/components/toggle/Toggle.component.js
@@ -9,85 +9,78 @@ type Props = {
   onChange: (e: SyntheticEvent<HTMLInputElement>) => void,
   label?: string,
 };
-
-const width = 20;
-const height = 10;
-
-const Switch = styled.label`
-  cursor: pointer;
-  & input {
-    width: 0;
-    height: 0;
-    margin: 0;
-    position: absolute;
-  }
-
-  & span {
-    display: block;
-    position: relative;
-    width: ${width}px;
-    height: ${height}px;
-    border-radius: ${width}px;
-    padding: 2px;
-    background-color: ${(props) =>
-  getTheme(props)[props.toggle ? 'selectedActive' : 'infoPrimary']};
-    transition: 0.4s;
-  }
-
-  & span:before {
-    content: '';
-    position: absolute;
-    inset: 0;
-    border: 1px solid transparent;
-    background-clip: padding-box;
-    background-color: ${(props) =>
-  getTheme(props)[props.toggle ? 'selectedActive' : 'backgroundLevel1']};
-    border-radius: ${width}px;
-    transition: 0.4s;
-  }
-
-  & span:after {
-    content: '';
-    display: block;
-    width: ${height}px;
-    height: 100%;
-    background-color: ${(props) =>
-  getTheme(props)[props.toggle ? 'textSecondary' : 'textTertiary']};
-    border-radius: 50%;
-    position: relative;
-    right: 0;
-    transition: 0.4s;
-  }
-
-  & input:checked + span:after {
-    right: calc(-100% + ${height}px);
-  }
-`;
-
 const ToggleContainer = styled.label`
   display: inline-flex;
   align-items: center;
   position: relative;
+  cursor: pointer;
+`;
+
+const Switch = styled.label`
+  cursor: pointer;
+  position: relative;
+  width: 24px;
+  height: 14px;
+`;
+
+const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${(props) => getTheme(props).backgroundLevel1};
+  border: 1px solid
+    ${(props) =>
+      getTheme(props)[props.toggle ? 'selectedActive' : 'infoPrimary']};
+  border-radius: 8px;
+  transition: 0.4s;
+  -moz-transform: rotate(0.02deg);
+  &:before {
+    border-radius: 100%;
+    position: absolute;
+    content: '';
+    height: 10px;
+    width: 10px;
+    left: 1px;
+    top: 1px;
+    background-color: ${(props) =>
+      getTheme(props)[props.toggle ? 'textSecondary' : 'textTertiary']};
+    transition: 0.4s;
+    -moz-transform: rotate(0.02deg);
+  }
+`;
+
+const ToggleInput = styled.input`
+  &:checked + ${Slider} {
+    background-color: ${(props) => getTheme(props).selectedActive};
+  }
+  &:checked + ${Slider}:before {
+    transform: translateX(10px);
+  }
+
+  display: none;
 `;
 
 const StyledSwitchLabel = styled.span`
   margin-left: ${defaultTheme.padding.smaller};
   font-size: ${defaultTheme.fontSize.base};
   color: ${(props) =>
-  getTheme(props)[props.toggle ? 'textPrimary' : 'textTertiary']};
+    getTheme(props)[props.toggle ? 'textPrimary' : 'textTertiary']};
 `;
 
 function ToggleSwitch({ toggle, label, onChange, ...rest }: Props) {
   return (
     <ToggleContainer className="sc-toggle">
-      <Switch toggle={toggle}>
-        <input
+      <Switch>
+        <ToggleInput
           type="checkbox"
           checked={toggle}
           onChange={onChange}
           {...rest}
         />
-        <span className="sc-slider"/>
+        <Slider className="sc-slider" toggle={toggle} />
       </Switch>
       {label && (
         <StyledSwitchLabel toggle={toggle} className="text">
