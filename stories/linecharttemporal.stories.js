3f518e045ca178ead6d77797be5c865ef19bfe94
YanJin <yanjin.cheng@scality.com>
2021-08-28 12:44:53 +0200

    stories: update the stories for the chart

--- stories/linecharttemporal.stories.js
+++ stories/linecharttemporal.stories.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { SyncedCursorCharts } from '../src/lib/components/vegachartv2/SyncedCursorCharts';
 import LineTemporalChart, {
   YAXIS_TITLE_READ_WRITE,
-  UNIT_RANGE_BS,
 } from '../src/lib/components/linetemporalchart/LineTemporalChart.component';
 import { MetricsTimeSpanProvider } from '../src/lib/components/linetemporalchart/MetricTimespanProvider.js';
 import { Wrapper } from './common';
@@ -17,38 +16,30 @@ export default {
   component: LineTemporalChart,
 };
 
-export const Default = () => (
-  <MetricsTimeSpanProvider>
+export const Default = () => {
+  return (
     <Wrapper>
-      <BrowserRouter>
-        <SyncedCursorCharts>
-          <div style={{ width: '600px' }}>
+      <MetricsTimeSpanProvider>
+        <BrowserRouter>
+          <SyncedCursorCharts>
             <LineTemporalChart
-              title={'ControalPlane Bandwidth'}
+              title={'CPU Usage'}
               series={dataLineChartV2}
-              unitRange={UNIT_RANGE_BS}
               height={300}
               yAxisType={'default'}
               startingTimeStamp={1629306229}
             />
-          </div>
-          <LineTemporalChart
-            title={'IOPS'}
-            series={dataLineChartV2_readwrite}
-            height={300}
-            yAxisType={'symmetrical'}
-            yAxisTitle={YAXIS_TITLE_READ_WRITE}
-            startingTimeStamp={1629306229}
-          />
-          <LineTemporalChart
-            title={'CPU Usage'}
-            series={dataLineChartV2}
-            height={300}
-            yAxisType={'percentage'}
-            startingTimeStamp={1629306229}
-          />
-        </SyncedCursorCharts>
-      </BrowserRouter>
+            <LineTemporalChart
+              title={'IOPS'}
+              series={dataLineChartV2_readwrite}
+              height={300}
+              yAxisType={'symmetrical'}
+              yAxisTitle={YAXIS_TITLE_READ_WRITE}
+              startingTimeStamp={1629306229}
+            />
+          </SyncedCursorCharts>
+        </BrowserRouter>
+      </MetricsTimeSpanProvider>
     </Wrapper>
-  </MetricsTimeSpanProvider>
-);
+  );
+};
