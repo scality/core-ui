53d333c2d8d83b708589cf83744cf3327104a78e
YanJin <yanjin.cheng@scality.com>
2021-08-30 22:21:52 +0200

    MetricTimespanProvider: Return all the useful value of timespan selector

--- src/lib/components/linetemporalchart/MetricTimespanProvider.js
+++ src/lib/components/linetemporalchart/MetricTimespanProvider.js
@@ -6,37 +6,45 @@ import { useLocation } from 'react-router-dom';
 import {
   SAMPLE_DURATION_LAST_TWENTY_FOUR_HOURS,
   SAMPLE_FREQUENCY_LAST_TWENTY_FOUR_HOURS,
+  QUERY_LAST_TWENTY_FOUR_HOURS,
+  LAST_TWENTY_FOUR_HOURS,
   queryTimeSpansCodes,
 } from '../constants.js';
 
 export type MetricsTimeSpanContextValue = {
+  label: string,
+  value: string,
   metricsTimeSpan: number,
   sampleFrequency: number,
 };
+
 export const MetricsTimeSpanContext = createContext<MetricsTimeSpanContextValue | null>(
   null,
 );
 
 export const MetricsTimeSpanProvider = ({ children }: { children: Node }) => {
-  // the value is the same as the sample duration of the last 24h
-  const [metricsTimeSpan, setMetricsTimeSpan] = useState(
-    SAMPLE_DURATION_LAST_TWENTY_FOUR_HOURS,
-  );
-  const [sampleFrequency, setSampleFrequency] = useState(
-    SAMPLE_FREQUENCY_LAST_TWENTY_FOUR_HOURS,
-  );
+  // the default timespan is the last 24h
+  const [queryTimeSpanCode, setQueryTimeSpanCode] = useState({
+    label: QUERY_LAST_TWENTY_FOUR_HOURS,
+    value: LAST_TWENTY_FOUR_HOURS,
+    duration: SAMPLE_DURATION_LAST_TWENTY_FOUR_HOURS,
+    frequency: SAMPLE_FREQUENCY_LAST_TWENTY_FOUR_HOURS,
+  });
   const urlSearchParams = new URLSearchParams(useLocation().search);
   const queryTimeSpan = urlSearchParams.get('from');
 
   // Sync url timespan to local timespan
   useEffect(() => {
     if (queryTimeSpan) {
-      const formatted = queryTimeSpansCodes.find(
+      const queryTimeSpanCode = queryTimeSpansCodes.find(
         (item) => item.label === queryTimeSpan,
       );
-      if (formatted && formatted.duration) {
-        setMetricsTimeSpan(formatted.duration);
-        setSampleFrequency(formatted.frequency);
+      if (queryTimeSpanCode && queryTimeSpanCode.label) {
+        setQueryTimeSpanCode(queryTimeSpanCode);
+      } else {
+        console.error(
+          'Unexpected timespan query parameter defined in the URL.',
+        );
       }
     }
   }, [queryTimeSpan]);
@@ -44,8 +52,10 @@ export const MetricsTimeSpanProvider = ({ children }: { children: Node }) => {
   return (
     <MetricsTimeSpanContext.Provider
       value={{
-        metricsTimeSpan,
-        sampleFrequency,
+        label: queryTimeSpanCode.label,
+        value: queryTimeSpanCode.value,
+        metricsTimeSpan: queryTimeSpanCode.duration,
+        sampleFrequency: queryTimeSpanCode.frequency,
       }}
     >
       {children}
@@ -54,6 +64,8 @@ export const MetricsTimeSpanProvider = ({ children }: { children: Node }) => {
 };
 
 export const useMetricsTimeSpan = (): {
+  label: string,
+  value: string,
   metricsTimeSpan: number,
   sampleFrequency: number,
 } => {
@@ -64,7 +76,12 @@ export const useMetricsTimeSpan = (): {
       "useMetricsTimeSpan hook can't be use outside <MetricsTimeSpanProvider/>",
     );
   }
-  const { metricsTimeSpan, sampleFrequency } = metricsTimeSpanContext;
+  const {
+    label,
+    value,
+    metricsTimeSpan,
+    sampleFrequency,
+  } = metricsTimeSpanContext;
 
-  return { metricsTimeSpan, sampleFrequency };
+  return { label, value, metricsTimeSpan, sampleFrequency };
 };
