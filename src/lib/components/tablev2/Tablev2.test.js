cdbea1abf33bdb5989abc83d42ffa986585f0d9d
Jean Gaudiau <gaudiauj@gmail.com>
2022-01-05 17:22:06 +0100

    improvement: test table component, mock the autoSizer rather than mock singleSelectableContent

--- src/lib/components/tablev2/Tablev2.test.js
+++ src/lib/components/tablev2/Tablev2.test.js
@@ -1,50 +1,17 @@
-import Table, { useTableContext } from './Tablev2.component';
+import Table from './Tablev2.component';
 import React from 'react';
 import { render } from '@testing-library/react';
 
-// this is the equivalent of table.SingleSelectableContentProps
-// but since the autoSizer from react-virtualized-auto-sizer is not working correctly in jest environment
-// we need to use this 'mock' to make the test working
-// this come directly from the basic example of react-table
-// https://react-table.tanstack.com/docs/examples/basic
-function TableChild() {
-  const {
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTableContext();
+jest.mock('./TableUtil', () => ({
+  ...jest.requireActual('./TableUtil'),
+  // since convertRemToPixels rely on getComputedStyle(document.documentElement) which is not available in jest
+  // we mock it
+  convertRemToPixels: () => 12,
+}));
 
-  return (
-    <>
-      <div>
-        {headerGroups.map((headerGroup) => (
-          <span {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <span {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </span>
-            ))}
-          </span>
-        ))}
-      </div>
-      <div {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <span {...row.getRowProps()} data-testid="row">
-              {row.cells.map((cell) => {
-                return (
-                  <span {...cell.getCellProps()}>{cell.render('Cell')}</span>
-                );
-              })}
-            </span>
-          );
-        })}
-      </div>
-    </>
-  );
-}
+jest.mock('react-virtualized-auto-sizer', () => ({ children }) => {
+  return children({ height: 600, width: 600 });
+});
 
 const data = [
   {
@@ -95,56 +62,68 @@ const columns = [
 
 describe('TableV2', () => {
   test('it should display all the data', async () => {
-    const { getAllByTestId } = render(
+    const { getAllByRole } = render(
       <div style={{ height: '400px' }}>
         <Table columns={columns} data={data} defaultSortingKey={'health'}>
-          <TableChild />
+          <Table.SingleSelectableContent
+            rowHeight="h40"
+            separationLineVariant="backgroundLevel3"
+            backgroundVariant="backgroundLevel1"
+          />
         </Table>
       </div>,
     );
 
     // we check that the table is displaying all the data
-    const rows = getAllByTestId('row');
-    expect(rows[3]).toHaveTextContent(/Ninette/i);
-    expect(rows[0]).toHaveTextContent(/yohann/i);
-    expect(rows.length).toBe(4);
+    const rows = getAllByRole('row');
+    expect(rows[4]).toHaveTextContent(/Ninette/i);
+    expect(rows[1]).toHaveTextContent(/yohann/i);
+    expect(rows.length).toBe(5);
   });
 
   test('it should sort by defaultSortingKey', async () => {
-    const { getAllByTestId } = render(
+    const { getAllByRole } = render(
       <div style={{ height: '400px' }}>
         <Table columns={columns} data={data} defaultSortingKey={'firstName'}>
-          <TableChild />
+          <Table.SingleSelectableContent
+            rowHeight="h40"
+            separationLineVariant="backgroundLevel3"
+            backgroundVariant="backgroundLevel1"
+          />
         </Table>
       </div>,
     );
 
     // we check that the table is displaying all the data
-    const rows = getAllByTestId('row');
-    expect(rows[0]).toHaveTextContent(/ninette/i);
-    expect(rows[3]).toHaveTextContent(/Yohann/i);
-    expect(rows.length).toBe(4);
+    const rows = getAllByRole('row');
+    expect(rows[1]).toHaveTextContent(/ninette/i);
+    expect(rows[4]).toHaveTextContent(/Yohann/i);
+    expect(rows.length).toBe(5);
   });
 
   test('it should filterGlobally', async () => {
-    const { getAllByTestId } = render(
+    const { getAllByRole } = render(
       <div style={{ height: '400px' }}>
         <Table
           columns={columns}
           data={data}
           defaultSortingKey={'firstName'}
-          globalFilter={'an'}
+          globalFilter="an"
         >
-          <TableChild />
+          <Table.SingleSelectableContent
+            rowHeight="h40"
+            separationLineVariant="backgroundLevel3"
+            backgroundVariant="backgroundLevel1"
+          />
         </Table>
       </div>,
     );
 
     // we check that the table is displaying all the data
-    const rows = getAllByTestId('row');
-    expect(rows[0]).toHaveTextContent(/an/i); //first name yoh-an-n
-    expect(rows[1]).toHaveTextContent(/an/i); //last name agath-an-gelou
-    expect(rows[2]).toHaveTextContent(/an/i); //first name stef-an-ia
-    expect(rows.length).toBe(3);
+    const rows = getAllByRole('row');
+    expect(rows[1]).toHaveTextContent(/an/i); //first name yoh-an-n
+    expect(rows[2]).toHaveTextContent(/an/i); //last name agath-an-gelou
+    expect(rows[3]).toHaveTextContent(/an/i); //first name stef-an-ia
+    expect(rows.length).toBe(4);
   });
 });
