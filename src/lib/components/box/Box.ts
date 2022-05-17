7acd71901f68e49316e1b831976b02f361c137f9
Patrick Ear <patrick.ear@scality.com>
2022-05-17 10:34:22 +0200

    Add box typing

--- src/lib/components/box/Box.ts
+++ src/lib/components/box/Box.ts
@@ -12,8 +12,31 @@ import {
   shadow,
   system,
 } from 'styled-system';
+import type {
+  LayoutProps,
+  FlexboxProps,
+  ColorProps,
+  SpaceProps,
+  PositionProps,
+  GridProps,
+  BackgroundProps,
+  BordersProps,
+  TypographyProps,
+  ShadowProps,
+} from 'styled-system';
 
-const Box = styled.div`
+type BoxComponentProps = LayoutProps &
+  FlexboxProps &
+  GridProps &
+  SpaceProps &
+  PositionProps &
+  ColorProps &
+  BackgroundProps &
+  BordersProps &
+  TypographyProps &
+  ShadowProps & { gap?: string | number };
+
+const Box = styled.div<BoxComponentProps>`
   ${layout}
   ${flexbox}
   ${grid}
