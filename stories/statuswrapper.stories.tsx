26dce99f456914597c546f5a6e32794efc876ec5
Jean-Marc Millet <jean-marcmillet@MacBook-Pro-de-SpareIT.local>
2023-10-20 14:36:06 +0200

    start update statuswrapper

--- stories/statuswrapper.stories.tsx
+++ stories/statuswrapper.stories.tsx
@@ -18,7 +18,22 @@ const PreviewWrapper = styled(Wrapper)`
 export default {
   title: 'Components/StatusWrapper',
   component: StatusWrapper,
+  decorators:[story => <Wrapper>{story()}</Wrapper>],
+  args:{
+    children:<Icon name={'Network'} color={'statusHealthy'} />
+  },
 };
+
+export const Playground = {}
+
+export const Statuses = {
+  render:() => {
+   return <StatusWrapper status='healthy' >
+      <Icon name={'Network'} size='3x' color={'statusHealthy'} />
+    </StatusWrapper>
+  }
+}
+
 export const Default = {
   render: ({}) => {
     return (
