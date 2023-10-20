ebc27bce8c3ee3e4ba89750d1b061f2e3cdc8000
Jean-Marc Millet <jean-marcmillet@MacBook-Pro-de-SpareIT.local>
2023-10-20 13:51:59 +0200

    update selectinput story

--- stories/selectinput.stories.tsx
+++ stories/selectinput.stories.tsx
@@ -3,10 +3,52 @@ import { Stack } from '../src/lib';
 import { Input } from '../src/lib/components/inputv2/inputv2';
 import { Select } from '../src/lib/components/selectv2/Selectv2.component';
 import { Wrapper } from './common';
+import styled from 'styled-components';
+
+const options = Array.from(new Array(10), (_, index) => (<Select.Option value={`Option${index}`} >{`Label-option ${index}`}</Select.Option>))
+
+const sizes = ["1/3","1/2","2/3","1"]
+
+const SelectWrapper = styled.div`
+display: flex;
+justify-content: space-around;
+`
 
 export default {
   title: 'Components/v2/SelectInput',
+  component:Select,
+  decorators:[story => <Wrapper>{story()}</Wrapper>],
+  args:{
+    children:options
+  },
+  argTypes:{
+    children:{
+      control:false
+    }
+  }
 };
+
+export const Playground = {}
+
+export const Disabled = {
+  args:{
+    disabled:true
+  }
+}
+
+export const RoundedVariant = {
+  args:{
+    variant:'rounded',
+  }
+}
+
+export const DifferentSizes = {
+  render:(args) => (
+    <SelectWrapper>
+      {sizes.map(size => <Select size={size} {...args} ></Select>)}
+    </SelectWrapper>
+  )
+}
 
 export const Default = {
   render: ({}) => (
