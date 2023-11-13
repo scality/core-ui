72d84d7c11cf40b18089aaef27b361b596a3b7c7
Patrick Ear <patrick.ear@scality.com>
2023-11-13 16:32:35 +0100

    Bump js temporal polyfill

--- src/lib/components/date/dateDiffer.ts
+++ src/lib/components/date/dateDiffer.ts
@@ -7,6 +7,6 @@ export function getDateDaysDiff(
 ) {
   const diff = Temporal.Duration.from({
     milliseconds: endDate.getTime() - startDate.getTime(),
-  }).total({ unit: unit, relativeTo: startDate.toISOString() });
+  }).total({ unit: unit, relativeTo: startDate.toISOString().split('T')[0] });
   return diff > 0 ? Math.floor(diff) : Math.round(diff);
 }
