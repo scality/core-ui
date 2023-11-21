6e94e41e6ad88f754dfb1176e298c19d99d6bdae
Jean-Marc Millet <jean-marc.millet@scality.com>
2023-11-21 11:56:37 +0100

    improve iconArgType, add it to chips

--- stories/controls.ts
+++ stories/controls.ts
@@ -2,24 +2,6 @@ import { iconTable } from "../src/lib/components/icon/Icon.component";
 
 export const iconOptions = Object.keys(iconTable);
 
-export const localeArgtype = {
-  control: { type: 'radio' },
-  options: ['en', 'fr'],
-  description: 'Set language for the component',
-};
-
-export const iconArgType = {
-  control: { type: 'select' },
-  options: iconOptions,
-  description: 'Icon to display in the button',
-};
-
-export const variantsOptions = [
-  'buttonPrimary',
-  'buttonSecondary',
-  'buttonDelete',
-  'backgroundLevel1',
-];
 export const sizesOptions = ['smaller', 'small', 'base', 'large', 'larger'];
 
 export const placementOptions = [
@@ -36,6 +18,28 @@ export const placementOptions = [
     'left-start',
     'left-end',
   ];
+export const localeArgtype = {
+  control: { type: 'radio' },
+  options: ['en', 'fr'],
+  description: 'Set language for the component',
+};
+
+export const iconArgType = {
+  control: { type: 'select' },
+  options: iconOptions,
+  description: 'Icon to display with the component',
+  table: {
+    type: { summary: 'Element' },
+  },
+};
+
+export const variantsOptions = [
+  'buttonPrimary',
+  'buttonSecondary',
+  'buttonDelete',
+  'backgroundLevel1',
+];
+
 
 export const tooltipArgTypes =  {
     tooltip: {
