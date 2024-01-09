2c36e9e2e2fbaf765038c6d4af22fc1e6a0cb9f8
Jean-Marc Millet <jean-marc.millet@scality.com>
2024-01-09 11:25:57 +0100

    add favicon

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,5 +1,12 @@
 import { addons } from '@storybook/manager-api';
 import Theme from './theme.storybook';
+
 addons.setConfig({
   theme: Theme,
 });
+
+// Add favicon
+const link = document.createElement('link');
+link.setAttribute('rel', 'shortcut icon');
+link.setAttribute('href', '/favicon.ico');
+document.head.appendChild(link);
