357449479c7a2ea1bacdbfa17a8037bd116e3770
Jean-Marc Millet <jean-marc.millet@scality.com>
2024-02-28 18:18:25 +0100

    increase size of search input in table

--- src/lib/components/tablev2/Search.tsx
+++ src/lib/components/tablev2/Search.tsx
@@ -99,7 +99,7 @@ export function TableSearch(props: SearchProps) {
         value={value}
         placeholder={translations[locale].search}
         disableToggle
-        size="2/3"
+        size="1"
         onChange={(evt) => {
           if (typeof onChange === 'function') {
             // @ts-ignore
