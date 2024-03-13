ae99ee8343952b554ce969523482f926a55cfc04
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2024-03-14 00:35:51 +0100

    GlobalHealthMonitoring: POC new implementation using Recharts

--- stories/globalhealthbar.stories.tsx
+++ stories/globalhealthbar.stories.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { GlobalHealthBar } from '../src/lib/components/globalhealthbar/GlobalHealthBar.component';
+import { GlobalHealthBar } from '../src/lib/components/globalhealthbar/GlobalHealthBarRecharts.component';
+import { GlobalHealthBar as VegaGlobalHealthBar } from '../src/lib/components/globalhealthbar/GlobalHealthBar.component';
 import { SyncedCursorCharts } from '../src/lib/components/vegachartv2/SyncedCursorCharts';
 import { Wrapper } from './common';
+import { Stack } from '../src/lib/spacing';
 const alerts = [
   {
     id: '1',
@@ -105,13 +107,18 @@ const endNotFirstDay = '2021-03-01T23:00:00Z';
 
 export default {
   title: 'Components/Data Display/Charts/GlobalHealthBar',
-  component: GlobalHealthBar,
+  component: (props) => (
+    <Stack direction="vertical">
+      <GlobalHealthBar {...props} />
+      <VegaGlobalHealthBar {...props} />
+    </Stack>
+  ),
   decorators: [
     (story) => (
       <Wrapper
         style={{ minHeight: '50vh', display: 'flex', alignItems: 'center' }}
       >
-        <div style={{ width: '500px' }}>
+        <div style={{ width: '500px', height: '400px' }}>
           <SyncedCursorCharts>{story()}</SyncedCursorCharts>
         </div>
       </Wrapper>
