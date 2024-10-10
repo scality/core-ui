5cdf7293dbc6e2ecaedd68cc3cef54b2826e7398
Jean-Marc Millet <jean-marc.millet@scality.com>
2024-10-10 18:19:38 +0200

    rename test file

--- src/lib/components/accordion/Accordion.test.tsx
+++ src/lib/components/accordion/Accordion.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { Accordion } from './accordion.component';
+import { Accordion } from './Accordion.component';
 import userEvent from '@testing-library/user-event';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
