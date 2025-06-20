bdd20d82788a421a6f324fb9ad78a98a913a9c6c
Patrick Ear <patrick.ear@outlook.com>
2025-06-20 19:27:38 +0200

    test tmp

--- stories/linecharttemporal.stories.tsx
+++ stories/linecharttemporal.stories.tsx
@@ -86,3 +86,123 @@ export const IOPS = {
     yAxisType: 'symmetrical',
   },
 };
+
+export const MyTest = {
+  render: (args) => {
+    const scm = {
+      title: 'Bandwidth',
+      series: [
+        {
+          resource: 'outband',
+          data: [
+            [1750343760, '0'],
+            [1750347360, '0'],
+            [1750350960, '0'],
+            [1750354560, '0'],
+            [1750401360, '0'],
+            [1750404960, '0'],
+            [1750408560, '0'],
+            [1750412160, '0'],
+            [1750415760, '0'],
+            [1750419360, '0'],
+            [1750422960, '0'],
+            [1750426560, '0'],
+            [1750430160, '0'],
+          ],
+          // getTooltipLabel: () => 'out',
+          // getLegendLabel: () => 'out',
+          getLegendLabel: (metricPrefix, resource) => {
+            return resource;
+          },
+          getTooltipLabel: (metricPrefix, resource) => {
+            return resource;
+          },
+        },
+        {
+          resource: 'in_band',
+          data: [
+            [1750343760, '0'],
+            [1750347360, '0'],
+            [1750350960, '0'],
+            [1750354560, '0'],
+            [1750401360, '0'],
+            [1750404960, '0'],
+            [1750408560, '0'],
+            [1750412160, '0'],
+            [1750415760, '0'],
+            [1750419360, '0'],
+            [1750422960, '149127'],
+            [1750426560, '0'],
+            [1750430160, '0'],
+          ],
+          // getTooltipLabel: () => 'in',
+          // getLegendLabel: () => 'in',
+          getLegendLabel: (metricPrefix, resource) => {
+            return resource;
+          },
+          getTooltipLabel: (metricPrefix, resource) => {
+            return resource;
+          },
+        },
+      ],
+      startingTimestamp: 1750343760,
+      height: 100,
+      startingTimeStamp: 1750343760,
+      isLoading: false,
+      unitRange: [
+        {
+          threshold: 0,
+          label: 'B/s',
+        },
+        {
+          threshold: 1024,
+          label: 'KiB/s',
+        },
+        {
+          threshold: 1048576,
+          label: 'MiB/s',
+        },
+        {
+          threshold: 1073741824,
+          label: 'GiB/s',
+        },
+        {
+          threshold: 1099511627776,
+          label: 'TiB/s',
+        },
+      ],
+    };
+    return (
+      <LineTemporalChart
+        {...args}
+        title={scm.title}
+        series={scm.series}
+        height={scm.height}
+        startingTimeStamp={scm.startingTimestamp}
+        isLoading={scm.isLoading}
+        unitRange={scm.unitRange}
+      />
+    );
+  },
+  args: {
+    title: 'CPU Usage',
+    yAxisType: 'default',
+    series: dataLineChartV2,
+    helpText: (
+      <>
+        This charts represents lorem ipsum
+        <br />
+        This charts represents lorem ipsum
+        <br />
+        This charts represents lorem ipsum
+        <br />
+        This charts represents lorem ipsum
+        <br />
+        This charts represents lorem ipsum
+        <br />
+        This charts represents lorem ipsum
+        <br />
+      </>
+    ),
+  },
+};
