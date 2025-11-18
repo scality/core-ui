288a273a6b260ed905ffc6cf0856adb7bc877483
Jean-Marc Millet <jean-marc.millet@scality.com>
2025-11-18 13:11:07 +0100

    Add tests for InfoMessage component to verify rendering with and without links

--- src/lib/components/infomessage/InfoMessageUtils.test.tsx
+++ src/lib/components/infomessage/InfoMessageUtils.test.tsx
@@ -1,8 +1,66 @@
+import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { coreUIAvailableThemes } from '../../style/theme';
 import { CoreUiThemeProvider } from '../coreuithemeprovider/CoreUiThemeProvider';
 import { useComputeBackgroundColor } from './InfoMessageUtils';
+import { InfoMessage } from './InfoMessage.component';
+import { getWrapper } from '../../testUtils';
+
+describe('InfoMessage', () => {
+  const selectors = {
+    title: () => screen.getByText('Title'),
+    content: () => screen.getByText('Content'),
+    defaultLinkText: () => screen.getByText('More info'),
+    link: () => screen.getByRole('link'),
+    linkText: () => screen.getByText('Link text'),
+  };
+  it('should render', () => {
+    const { Wrapper } = getWrapper();
+    render(<InfoMessage title="Title" content="Content" />, {
+      wrapper: Wrapper,
+    });
+    expect(selectors.title()).toBeInTheDocument();
+    expect(selectors.content()).toBeInTheDocument();
+  });
+  it('should render with link and default link text', () => {
+    const { Wrapper } = getWrapper();
+    render(
+      <InfoMessage
+        title="Title"
+        content="Content"
+        link="https://www.google.com"
+      />,
+      {
+        wrapper: Wrapper,
+      },
+    );
+    expect(selectors.title()).toBeInTheDocument();
+    expect(selectors.content()).toBeInTheDocument();
+    expect(selectors.link()).toBeInTheDocument();
+    expect(selectors.defaultLinkText()).toBeInTheDocument();
+    expect(selectors.link()).toHaveAttribute('href', 'https://www.google.com');
+  });
+  it('should render with correct link text', () => {
+    const { Wrapper } = getWrapper();
+    render(
+      <InfoMessage
+        title="Title"
+        content="Content"
+        link="https://www.google.com"
+        linkText="Link text"
+      />,
+      {
+        wrapper: Wrapper,
+      },
+    );
+    expect(selectors.title()).toBeInTheDocument();
+    expect(selectors.content()).toBeInTheDocument();
+    expect(selectors.link()).toBeInTheDocument();
+    expect(selectors.linkText()).toBeInTheDocument();
+    expect(selectors.link()).toHaveAttribute('href', 'https://www.google.com');
+  });
+});
 
 describe('useComputeBackgroundColor', () => {
   const SUT = jest.fn();
