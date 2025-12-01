ec3da7bc693c736ed5226722f5ccdba4a8cb3a30
YanJin <yanjin.cheng@scality.com>
2025-12-01 10:09:52 +0100

    Pass react node to text badge

--- stories/textbadge.stories.tsx
+++ stories/textbadge.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { TextBadge } from '../src/lib/components/textbadge/TextBadge.component';
 import { Wrapper, Title } from './common';
+import { IconHelp } from '../src/lib/components/iconhelper/IconHelper';
+import { Text } from '../src/lib/components/text/Text.component';
 export default {
   title: 'Components/TextBadge',
   component: TextBadge,
@@ -21,6 +23,19 @@ export const Default = {
         <TextBadge text="2" variant="statusWarning" />
         <TextBadge text="3" variant="statusCritical" />
         <TextBadge text="Badge" variant="infoSecondary" />
+        <TextBadge
+          text={
+            <>
+              <Text>This is a badge with a tooltip</Text>
+              <span style={{ marginLeft: '8px' }}>
+                <IconHelp
+                  tooltipMessage={<div>This is a tooltip</div>}
+                  title="Info"
+                />
+              </span>
+            </>
+          }
+        />
       </Wrapper>
     );
   },
